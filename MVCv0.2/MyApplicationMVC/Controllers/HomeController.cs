788ff66ff2dfecf11ad1c7981aad47d601029160
Aleksey <1benzine1@gmail.com>
2018-11-10 23:48:07 +0200

    MVC v0.2

---
+++ MVCv0.2/MyApplicationMVC/Controllers/HomeController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MyApplicationMVC.Models;
+
+namespace MyApplicationMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        LogStringParts logStringParts = new LogStringParts();
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        //private bool isValidContentType(string contentType)
+        //{
+        //    return contentType.Equals("string");
+        //}
+
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase upload)
+        {
+            if (upload == null /*|| (!isValidContentType(upload.ContentType))*/)
+            {
+                ViewBag.Check = "error";
+
+                return View("Parser");
+            }
+
+            if (upload.ContentLength > 0)
+            {
+                Tools tools = new Tools();
+                var fileName = Path.GetFileName(upload.FileName);
+                var path = Path.Combine(Server.MapPath("LogFile/"), fileName);
+                upload.SaveAs(path);
+                List<LogStringParts> buffer = tools.ReadFile(path);
+                //for(int i = 1; i< buffer.Count;i++)
+                //{
+                //    foreach(var el in buffer)
+                //    {
+                //        el.Id;
+                //    }
+                //}
+
+
+                ViewBag.Strings = buffer;
+            }
+            return View("Parser");
+        }
+
+        [HttpGet]
+        public ActionResult Parser()
+        {
+            ViewBag.Message = "Parser page.";
+
+            return View();
+        }
+
+        public ActionResult IpInfo(string ip)
+        {
+            Tools tools = new Tools();
+            object buffer = tools.ShowIpInfo(ip);
+            ViewBag.Ip = ip;
+
+            return View(buffer);
+        }
+    }
+}
---
+++ MVCv0.2/MyApplicationMVC/Models/Tools.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Net;
+
+namespace MyApplicationMVC.Models
+{
+    public class Tools
+    {
+        int id = 1;
+        public List<LogStringParts> RegexMethod(string pattern, string line)
+        {
+            LogStringParts logStrPart = new LogStringParts();
+            List<LogStringParts> listLogParts = new List<LogStringParts>();
+            Regex regex = new Regex(pattern);
+            Match match = regex.Match(line);
+            string strReplace = null;
+            string[] replaceableChars = new string[6] { "- ", "\"", "[", "]", "HTTP/1.1", "HTTP/1.0" };
+
+            while (match.Success)
+            {
+                strReplace = match.Value;
+              
+                for (int i = 0; i < replaceableChars.Length; i++)
+                {
+                    strReplace = strReplace.Replace(replaceableChars[i], "");
+                }
+
+                string[] words = strReplace.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                listLogParts.Add(new LogStringParts
+                {
+                    Id = id,
+                    Ip = words[0],
+                    DateTime = words[1],
+                    TimeZone = words[2],
+                    TypeRequest = words[3],
+                    FileDownload = words[4],
+                    StatusCode = Convert.ToInt32(words[5]),
+                    SizeTransmitDate = Convert.ToInt32(words[6])
+                });
+
+                id++;
+
+                match = match.NextMatch();
+            }
+
+            return listLogParts;
+        }
+        // Добавить прокси для обхода блокировки лимита запросов на проверку ip.
+        public object ShowIpInfo(string ip)
+        {
+                HttpClient client = new HttpClient();
+                var json = client.GetStringAsync("http://ip-api.com/json/" + ip +
+                                                    "?fields=status,message,country,city,org").Result;
+                var ipinf = JsonConvert.DeserializeObject<IpInfo>(json);
+                ipinf.ip = ip;
+
+                return ipinf;
+        }
+
+        public List<LogStringParts> ReadFile(string pathLogFile)
+        {
+            List<LogStringParts> result = null;
+                using (StreamReader sr = new StreamReader(pathLogFile, System.Text.Encoding.Default))
+                {
+                    Tools tools = new Tools();
+                    int check = 0;
+                    string pattern = @"^((\d{1,3}\.){3}\d{1,3}).+\[(.+)].+?\""(\w+)\s+(\S+).+?\"".+?(\d{3}).+?(\d+)";
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        if (check == 3)
+                            break;
+
+                        result = tools.RegexMethod(pattern, line);
+
+                        check++;
+                    }
+                }
+            //if(result == null) result = new List<LogStringParts>(1);
+            return result;
+        }
+    }
+}
